eb2f02fbdae3559f8abe6d2017f298872276b91d
m010k0 <antona89@yandex.ru>
2023-04-10 16:59:57 +0300

    Написал маршрутизатор

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 if (PHP_MAJOR_VERSION < 8) die('Необходима версия PHP 8 или более');
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \wfm\App();
-
-throw new Exception('Возникла ошибка', 500);
--- vendor/wfm/App.php
+++ vendor/wfm/App.php
@@ -9,9 +9,12 @@ class App
 
     public function __construct()
     {
+        $query = trim(urldecode($_SERVER['QUERY_STRING']), '/');
         new ErrorHandler();
         self::$app = Registry::getInstance();
         $this->getParams();
+        Router::dispatch($query);
+
     }
 
     protected function getParams()
@@ -23,4 +26,7 @@ class App
             }
         }
     }
+
+
+
 }
